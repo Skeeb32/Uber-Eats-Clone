ad146e9dc13876879684091a73f294515548ba45
Skeeb32 <skeeb32@gmail.com>
2022-12-20 05:04:34 -0600

    Google Place API

--- screens/Home.js
+++ screens/Home.js
@@ -9,11 +9,12 @@ const YELP_API_KEY =
   "O_JsAQNFSq9qjQR0nDFJGMtQsbXZC9X7KlTXGkIKiCIISTe1G-qdbvUkbot6p_3oi34TY23ymRmUIx6RPXG_fZg5SvwUPuoH6DASMoah3e_BzBnWIOllR8vNlomhY3Yx"; 
 
 export default function Home() {
-  const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState("San Francisco");
 
   const getRestaurantsFromYelp = () => {
     const yelpUrl = 
-    "https://api.yelp.com/v3/businesses/search?term=restaurants&location=Hollywood";
+    `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
 
     const apiOptions = {
       headers: {
@@ -30,13 +31,13 @@ export default function Home() {
 
   useEffect(() => { 
     getRestaurantsFromYelp();
-  }, []);
+  }, [city]);
 
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1}}>
       <View style={{backgroundColor: "white", padding: 15}}>       
         <HeaderTabs />
-        <SearchBar /> 
+        <SearchBar cityHandler={setCity} /> 
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories /> 
